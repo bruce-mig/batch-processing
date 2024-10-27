6c02b59cec7ec6e95cde13b46403f7fdea9368ff
bruce-mig <brucetmig@icloud.com>
2024-10-27 22:52:26 +0200

    added lombok constructor annotations

--- src/main/java/com/github/bruce_mig/batch_processing/student/Student.java
+++ src/main/java/com/github/bruce_mig/batch_processing/student/Student.java
@@ -3,18 +3,22 @@ package com.github.bruce_mig.batch_processing.student;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
 
     @Id
     @GeneratedValue
     private Integer id;
-    private String fistName;
+    private String firstName;
     private String lastName;
     private int age;
 }
